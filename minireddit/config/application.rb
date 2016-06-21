7c19269206cf93c4bcf2b5ef0e05b483702ef65b
Sergio Diniz <sergiodinizsh@gmail.com>
2016-06-21 10:13:09 -0300

    creat User with devise gem

--- minireddit/config/application.rb
+++ minireddit/config/application.rb
@@ -22,5 +22,11 @@ module Minireddit
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Config for devise
+    # 4. 
+    # If you are deploying on Heroku with Rails 3.2 only, you may want to set:
+    # forcing your application to not access the DB or load models when precompiling your assets.
+    config.assets.initialize_on_precompile = false
   end
 end
