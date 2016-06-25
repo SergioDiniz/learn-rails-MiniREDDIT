8f7a662414b5b7bf40f5a89b560c8d8d22f52a16
Sergio Diniz <sergiodinizsh@gmail.com>
2016-06-25 12:10:35 -0300

    add button like e unlike with acts as votable gem

--- minireddit/db/schema.rb
+++ minireddit/db/schema.rb
@@ -11,16 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622113951) do
+ActiveRecord::Schema.define(version: 20160625141637) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.integer  "user_id"
+    t.integer  "cached_votes_total",      default: 0
+    t.integer  "cached_votes_score",      default: 0
+    t.integer  "cached_votes_up",         default: 0
+    t.integer  "cached_votes_down",       default: 0
+    t.integer  "cached_weighted_score",   default: 0
+    t.integer  "cached_weighted_total",   default: 0
+    t.float    "cached_weighted_average", default: 0.0
   end
 
+  add_index "links", ["cached_votes_down"], name: "index_links_on_cached_votes_down"
+  add_index "links", ["cached_votes_score"], name: "index_links_on_cached_votes_score"
+  add_index "links", ["cached_votes_total"], name: "index_links_on_cached_votes_total"
+  add_index "links", ["cached_votes_up"], name: "index_links_on_cached_votes_up"
+  add_index "links", ["cached_weighted_average"], name: "index_links_on_cached_weighted_average"
+  add_index "links", ["cached_weighted_score"], name: "index_links_on_cached_weighted_score"
+  add_index "links", ["cached_weighted_total"], name: "index_links_on_cached_weighted_total"
   add_index "links", ["user_id"], name: "index_links_on_user_id"
 
   create_table "users", force: :cascade do |t|
@@ -41,5 +55,20 @@ ActiveRecord::Schema.define(version: 20160622113951) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
 
 end
